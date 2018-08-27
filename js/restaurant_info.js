a97586492c2df121fd05ad61b3ef9d6bd102b210
Karen Soriano <karensoriano@karens-mbp.attlocal.net>
2018-08-26 20:41:55 -0500

    Removed API key 8/26

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1Ijoia2FyZW45MTAxMyIsImEiOiJjamwwemI4bWExYWR6M3BvZW8wY3JnYjF0In0.IRz39fmn6sGC-rZbIcJW1w',
+        mapboxToken: '<your MAPBOX API KEY HERE>',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
